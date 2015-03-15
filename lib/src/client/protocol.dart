09c721a39a752fb8695b9e1265e28badf225f355
Robert Hartung <r.hartung@roberthartung.de>
2015-03-15 14:50:57 +0100

    Protocol documentation added

--- lib/src/client/protocol.dart
+++ lib/src/client/protocol.dart
@@ -7,32 +7,53 @@
 part of webrtc_utils.client;
 
 /**
- * Abstract
+ * Protocol interface: send method and message stream
  */
 
 abstract class DataChannelProtocol<M> {
+  Stream<M> get onMessage;
   void send(dynamic data);
 }
 
 /**
- * Raw protocol that deliver's the raw messages
+ * Raw protocol that deliver's the raw messages as they come in.
+ * 
+ * NOTE: Chrome does not supporting sending Blobs at the moment so sending Blogs might fail at the moment
  */
 
 class RawProtocol<M> implements DataChannelProtocol<M> {
+  /**
+   * The [RtcDataChannel] instance to send message to and receive messages from
+   */
+  
   final RtcDataChannel channel;
+  
+  /**
+   * Concrete message stream
+   */
   
   Stream<M> get onMessage => _onMessageController.stream;
   StreamController<M> _onMessageController = new StreamController<M>.broadcast();
   
+  /**
+   * Constructor
+   */
+  
   RawProtocol(this.channel) {
-    channel.onMessage.listen((MessageEvent ev) {
-      _onMessage(ev.data);
-    });
+    channel.onMessage.listen((MessageEvent ev) => _onMessage(ev.data));
   }
   
-  void _onMessage(dynamic data) {
+  /**
+   * Internal function that listens for incoming messages
+   */
+  
+  void _onMessage(Object data) {
     _onMessageController.add(data);
   }
+  
+  /**
+   * Sends a message [M] to the channel
+   */
   
   void send(M message) {
     channel.send(message);
@@ -48,7 +69,8 @@ class StringProtocol extends RawProtocol<String> {
 }
 
 /**
- *
+ * Interface that provides an instance of [DataChannelProtocol]
+ * Assignment from protocol to [ProtocolProvider] is done using [P2PClient.addProtocolProvider]
  */
 
 abstract class ProtocolProvider {
