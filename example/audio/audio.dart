4d33e10754de3a4590fae0716b8faa839a7e6519
Robert Hartung <r.hartung@roberthartung.de>
2015-03-29 13:51:32 +0200

    Moved examples to their own directories

--- example/audio/audio.dart
+++ example/audio/audio.dart
@@ -1,55 +1,52 @@
 import 'package:webrtc_utils/client.dart';
 import 'dart:html';
 
-// const Map rtcConfiguration = const {"iceServers": const [ const {"url": "stun:stun.l.google.com:19302"}]};
 final String url = 'ws://${window.location.hostname}:28080';
-P2PClient client;
-
-void _onAddStream(Peer peer, MediaStream ms) {
-  print('Stream added from Peer#${peer.id}: $ms');
-  VideoElement video = new VideoElement();
-  video.width = 300;
-  video.autoplay = true;
-  video.controls = true;
-  video.src = Url.createObjectUrlFromStream(ms);
-  document.body.append(video);
-}
+MediaStream localStream;
+bool share = false;
 
 void _onPeerAdded(Peer peer) {
   print('Peer $peer joined');
-  
-  peer.onAddStream.listen((MediaStreamEvent ev) => _onAddStream(peer, ev.stream));
-  
+  peer.onAddStream.listen((MediaStreamEvent ev) => appendStream(ev.stream));
   if(share) {
     peer.addStream(localStream);
   }
 }
 
-MediaStream localStream;
-bool share = false;
-
-/**
- * Wasn't able to share first time
- */
+MediaElement appendStream(MediaStream ms) {
+  // Check type and create video or audio
+  if(ms.getVideoTracks().length > 0) {
+    VideoElement video = new VideoElement();
+    video.width = 300;
+    video.autoplay = true;
+    video.controls = true;
+    video.src = Url.createObjectUrlFromStream(ms);
+    document.body.append(video);
+    return video;
+  } else if(ms.getAudioTracks().length > 0) {
+    AudioElement audio= new AudioElement();
+    audio.autoplay = true;
+    audio.controls = true;
+    audio.src = Url.createObjectUrlFromStream(ms);
+    document.body.append(audio);
+    return audio;
+  }
+  
+  throw "MediaStream $ms has no video or audio streams.";
+}
 
 void main() {
-  final UListElement peerList = querySelector('#peers');
-  client = new WebSocketP2PClient(url, rtcConfiguration);
+  P2PClient client = new WebSocketP2PClient(url, rtcConfiguration);
   
   ButtonElement shareButton = querySelector('#share');
   shareButton.disabled = true;
   
   client.onConnect.listen((final int id) {
-    window.navigator.getUserMedia(audio: true, video: true).catchError((error) {
+    window.navigator.getUserMedia(audio: true).catchError((error) {
       window.alert('Error when initializing: $error');
     }).then((MediaStream ms) {
       localStream = ms;
-      VideoElement video = new VideoElement();
-      video.width = 300;
-      video.autoplay = true;
-      video.muted = true;
-      video.src = Url.createObjectUrlFromStream(localStream);
-      document.body.append(video);
+      appendStream(ms).muted = true;
       client.join('audio');
     });
   });
@@ -62,16 +59,10 @@ void main() {
         peer.addStream(localStream);
       });
     });
-    
-    print('I joined room ${room.name} with peers ${room.peers}');
-    
-    // Loop through existing peers
     room.peers.forEach(_onPeerAdded);
-    
     room.onPeerLeave.listen((Peer peer) {
       print('Peer $peer left room ${room.name}');
     });
-    
     room.onPeerJoin.listen((Peer peer) {
       _onPeerAdded(peer);
     });
