891386379b13c3075ec65970d1ee0dd6eaf3b75f
Robert Hartung <r.hartung@roberthartung.de>
2015-04-04 11:21:06 +0200

    IceServerList can be used from the client package.

--- example/chrome_app/app.dart
+++ example/chrome_app/app.dart
@@ -8,30 +8,6 @@ import 'package:webrtc_utils/client.dart';
   // , "omnibox"
 */
 
-const List iceServers = const [
-  const {'url':'stun:stun01.sipphone.com'},
-  const {'url':'stun:stun.ekiga.net'},
-  const {'url':'stun:stun.fwdnet.net'},
-  const {'url':'stun:stun.ideasip.com'},
-  const {'url':'stun:stun.iptel.org'},
-  const {'url':'stun:stun.rixtelecom.se'},
-  const {'url':'stun:stun.schlund.de'},
-  const {'url':'stun:stun.l.google.com:19302'}, 
-  const {'url':'stun:stun1.l.google.com:19302'},
-  const {'url':'stun:stun2.l.google.com:19302'},
-  const {'url':'stun:stun3.l.google.com:19302'},
-  const {'url':'stun:stun4.l.google.com:19302'},
-  const {'url':'stun:stunserver.org'},
-  const {'url':'stun:stun.softjoys.com'},
-  const {'url':'stun:stun.voiparound.com'},
-  const {'url':'stun:stun.voipbuster.com'},
-  const {'url':'stun:stun.voipstunt.com'},
-  const {'url':'stun:stun.voxgratia.org'},
-  const {'url':'stun:stun.xten.com'}
-];
-
-const Map rtcConfiguration = const {"iceServers": iceServers};
-
 final String url = 'ws://roberthartung.dyndns.org:28080';
 P2PClient client;
 
@@ -43,13 +19,13 @@ void _peerJoined(Peer peer) {
       print('No access');
       return;
     }
-    
+
     print('streamId: $streamId');
     window.navigator.getUserMedia(video: {'mandatory': {'maxWidth': 1920, 'maxHeight': 1080, 'minFrameRate': 15, 'maxFrameRate': 30, 'chromeMediaSource': "desktop", 'chromeMediaSourceId': streamId }}).then((MediaStream ms) {
       peer.addStream(ms);
       ms.getTracks().forEach((MediaStreamTrack track) {
         print('Track $track ${track.kind} ${track.id} ${track.label}');
-        
+
       });
       VideoElement video = querySelector('#preview');
       video.autoplay = true;
@@ -61,24 +37,24 @@ void _peerJoined(Peer peer) {
 void main() {
   print('loaded');
   client = new WebSocketP2PClient(url, rtcConfiguration);
-  
+
   client.onConnect.listen((localId) {
     print('I am connected. Joining room.');
     client.join('demo');
   });
-  
+
   client.onJoinRoom.listen((PeerRoom room) {
     print('Joined room');
     room.peers.forEach(_peerJoined);
     room.onPeerJoin.listen(_peerJoined);
   });
-  
+
   /*
   identity.getAccounts().then((List<AccountInfo> accounts) {
     print(accounts);
   });
   */
-  
+
   identity.onSignInChanged.listen((OnSignInChangedEvent ev) {
     if(ev.signedIn) {
       identity.getProfileUserInfo().then((ProfileUserInfo info) {
